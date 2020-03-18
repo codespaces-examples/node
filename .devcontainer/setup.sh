061999ca9da718689cf726dac372090d30ea3e07
Tierney Cyren <hello@bnb.im>
2020-03-18 17:18:28 -0400

    fix: fixes to zsh and config setup, adding `developer` user

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,15 @@
 apt-get update
-apt-get install -y
+apt-get install -y \
+  curl \
+  git \
+  jq \
+  zsh
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+useradd -m developer
+su developer
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get install yarn -y --no-install-recommends
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
