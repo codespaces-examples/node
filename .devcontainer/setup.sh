345ebda534bb456fbcb024fa4d81754934bba25b
Tierney Cyren <hello@bnb.im>
2020-03-19 13:26:40 -0400

    fix: potentially fix yarn registry issue

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,8 +8,8 @@ apt-get install -y \
 useradd -m developer
 su developer
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt-get install yarn -y --no-install-recommends
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
