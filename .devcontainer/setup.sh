707d59aea45a6368e9dcd768b825eddd03320f4f
Tierney Cyren <hello@bnb.im>
2020-04-23 15:26:51 -0400

    chore: Add comments and slightly restructure setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,4 @@
+## update and install some things we should probably have
 apt-get update
 apt-get install -y \
   curl \
@@ -5,17 +6,21 @@ apt-get install -y \
   jq \
   zsh
 
+## set-up and install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 apt-get install yarn -y --no-install-recommends
 
+## instsall nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+## add 'developer' and make them a superusers
 useradd -m developer
 su developer
 
+## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrcs
