e1d2d2c169c6b3186a620bab2167d60203e2ba58
Tierney Cyren <hello@bnb.im>
2020-04-23 16:20:40 -0400

    fix: revert some changes that break the build and add some deps

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,17 @@
-## set-up yarn's deb registry
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
 ## update and install some things we should probably have
 apt-get update
-apt-get install -y --no-install-recommends \
+apt-get install -y \
   curl \
   git \
+  gnupg2 \
   jq \
-  yarn \
+  sudo \
   zsh
+
+## set-up and install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get install yarn -y --no-install-recommends
 
 ## instsall nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -19,4 +21,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrcsx`
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
