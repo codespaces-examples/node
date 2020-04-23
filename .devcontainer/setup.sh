2e035ec28913515e6dd577acaf5ce0a3fd4ffce1
Tierney Cyren <hello@bnb.im>
2020-04-23 16:07:26 -0400

    chore: further simplifications

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,26 +1,22 @@
+## set-up yarn's deb registry
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 ## update and install some things we should probably have
 apt-get update
-apt-get install -y \
+apt-get install -y --no-install-recommends \
   curl \
   git \
   jq \
+  yarn \
   zsh
-
-## set-up and install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get install yarn -y --no-install-recommends
 
 ## instsall nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-## add 'developer' and make them a superusers
-useradd -m developer
-su developer
 
 ## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrcs
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrcsx`
