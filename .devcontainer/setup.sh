b3339fe239394b4cadd94185a732de10e1a6cdd2
Tierney Cyren <hello@bnb.im>
2020-04-30 16:16:04 -0400

    fix: add export nvm

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,6 +16,9 @@ apt-get install yarn -y --no-install-recommends
 ## instsall nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 ## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
