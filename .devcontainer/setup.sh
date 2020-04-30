e1f074be4b003a314808d7cf2ce6f76208345593
Tierney Cyren <hello@bnb.im>
2020-04-30 16:22:21 -0400

    fix: maybe address incorrect "yarn" being installed

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,9 +9,9 @@ apt-get install -y \
   zsh
 
 ## set-up and install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get install yarn -y --no-install-recommends
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install yarn -y
 
 ## instsall nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
