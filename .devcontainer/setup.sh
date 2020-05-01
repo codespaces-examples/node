95fcfcc4a8a756c067dc429ade516a72dcee93d5
Tierney Cyren <hello@bnb.im>
2020-05-01 14:39:41 -0400

    fix: potentially address nvm access

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,7 +22,3 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-## add nvm to ~/.zshrc
-echo "export NVM_DIR=\"$HOME/.nvm\"" >> ~/.zshrc
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> ~/.zshrc
