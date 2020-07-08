02a1aad00fbe9c84afcb5dbfda7a60558e9e4c36
Tierney Cyren <hello@bnb.im>
2020-07-08 11:56:28 -0400

    feat: add `hub` and `gh` CLIs

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,19 +4,23 @@ apt-get install -y \
   curl \
   git \
   gnupg2 \
+  hub \
   jq \
   sudo \
   zsh
 
-## set-up and install yarn
+# set-up and install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get install yarn -y
 
-## instsall nvm
+# instsal nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-## setup and install oh-my-zsh
+# install `gh` CLI
+./install-gh-cli.sh
+
+# setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
