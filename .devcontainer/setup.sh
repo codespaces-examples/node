887dc008d480cca9541a9719ee02ea2c1b39a80f
Tierney Cyren <hello@bnb.im>
2020-08-24 11:46:53 -0400

    chore: add comments to setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,15 +8,15 @@ apt-get install -y \
   sudo \
   zsh
 
-## set-up and install yarn
+# set-up and install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get install yarn -y
 
-## instsall nvm
+# instsal nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-## setup and install oh-my-zsh
+# setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
